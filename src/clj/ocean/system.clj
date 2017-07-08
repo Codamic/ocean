5b5f055289d25449e18cc9e19e33df176fac1f9e
Sameer Rahmani <lxsameer@gnu.org>
2017-07-08 14:46:15 +0100

    websocket component added to the system

--- src/clj/ocean/system.clj
+++ src/clj/ocean/system.clj
@@ -1,9 +1,13 @@
 (ns ocean.system
   (:require
    [ocean.service                 :as service]
-   [hellhound.components.pedestal :as pedestal]
+   [hellhound.components
+    [websocket :as websocket]
+    [pedestal  :as pedestal]]
+
    [hellhound.system              :refer [defsystem]]))
 
+(def event-router {})
 (defsystem dev-system
-  (websocket/make-instanc)
-  (pedestal/make-instance service/service))
+  (websocket/make-instance {:router event-router})
+  (pedestal/make-instance #'service/service))
