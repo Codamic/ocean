f60f949b6c5843eb532ed3be11f455fc24cbca64
Sameer Rahmani <lxsameer@gnu.org>
2017-07-09 11:56:47 +0100

    re-frame source structure added to client side application

---
+++ src/cljs/ocean/db.cljs
@@ -0,0 +1,6 @@
+(ns ocean.db)
+
+(def default-db
+  {:lang "en"
+   :recent-articles []
+   :nav {:section-title "Latest Stories"}})
---
+++ src/cljs/ocean/views.cljs
@@ -0,0 +1,16 @@
+(ns ocean.views
+  (:require [re-frame.core        :as re-frame]
+            [oneiri.views.home   :refer [home]]))
+
+(defn- panels [panel-name]
+  (case panel-name
+    :home-panel [home]
+    [:div]))
+
+(defn show-panel [panel-name]
+  [panels panel-name])
+
+(defn main-panel []
+  (let [active-panel (re-frame/subscribe [:active-panel])]
+    (fn []
+      [show-panel @active-panel])))
