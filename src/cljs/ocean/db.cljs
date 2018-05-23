20922591b394af1e5a3dc24740a15a4bfbb91df7
Sameer Rahmani <lxsameer@gnu.org>
2018-05-23 23:27:05 +0100

    old Ocean application has been removed

--- src/cljs/ocean/db.cljs
+++
@@ -1,6 +0,0 @@
-(ns ocean.db)
-
-(def default-db
-  {:lang "en"
-   :recent-articles []
-   :nav {:section-title "Latest Stories"}})
--- src/cljs/ocean/views/home.cljs
+++
@@ -1,5 +0,0 @@
-(ns ocean.views.home)
-
-(defn home
-  []
-  [:h1 "Hello World"])
