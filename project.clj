b5a3bba66dd9b401bc4ff75edeb190b623ec789f
Sameer Rahmani <lxsameer@gnu.org>
2018-05-23 23:30:48 +0100

    The Onyx application has been bootstrapped

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject ocean "0.1.0-SNAPSHOT"
+  :description ""
+  :url ""
+  :license {:name ""
+            :url ""}
+  :dependencies [[aero "1.0.3"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.cli "0.3.5"]
+                 [org.onyxplatform/onyx "0.12.4"]
+                 [org.onyxplatform/lib-onyx "0.12.0.0"]]
+  :source-paths ["src"]
+
+  :profiles {:dev {:jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
+                   :global-vars {*assert* true}
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                                  [lein-project-version "0.1.0"]]}
+
+             :uberjar {:aot [lib-onyx.media-driver
+                             ocean.core]
+                       :uberjar-name "peer.jar"
+                       :global-vars {*assert* false}}})
